853e6c5600c29575035cd7501ca4c3dd5615239f
Ancha <80835866+oolle4043@users.noreply.github.com>
2024-02-15 10:13:47 +0900

    Add files via upload

---
+++ server/error_handling.h
@@ -0,0 +1,6 @@
+#ifndef ERROR_HANDLING_H
+#define ERROR_HANDLING_H
+
+void error_hanling(void *arg);
+
+#endif
---
+++ server/globalVariable.c
@@ -0,0 +1,5 @@
+#include "globalVariable.h"
+
+pthread_mutex_t mutx;
+int clnt_cnt;
+int clnt_socks[MAX_CLNT];
